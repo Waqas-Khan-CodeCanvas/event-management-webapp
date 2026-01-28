950b93bc1eec03602f8d878ee1af87ab15b4df4a
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-01-28 19:56:23 +0500

    backned logic

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,48 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-require("dotenv").config();
-const usersData = require("./MOCK_DATA.json");
-const fs = require('fs');
-// middlewares 
+const mongoose = require("mongoose");
+const {port} = require('./config/env.js')
+const connectDB = require('./config/db.js');
+connectDB()
+// middlewares
 express(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
-// GET all users in html 
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    gender: {
+      type: String,
+    },
+    jobType: {
+      type: String,
+    },
+  },
+  { timeStamp: true },
+);
+
+const User = mongoose.model("users", userSchema);
+
+// GET all users in html
 app.get("/users", (req, res) => {
-    const html = `
+  const html = `
         <h1>Users</h1>
-        <div">${usersData.map((user) => `
+        <div">${usersData
+          .map(
+            (user) => `
             <ul>
                 <li>S.NO :${user.id} </li>
                 <li>First Name :${user.first_name} </li>
@@ -20,59 +51,50 @@ app.get("/users", (req, res) => {
                 <li>Gender :${user.gender}</li>
                 <li>Job Type :${user.job_type}</li>
             </ul>
-            `).join("")}
+            `,
+          )
+          .join("")}
         </div>`;
-    res.send(html);
+  res.send(html);
 });
 
-// GEt user by id 
-app.get("/users/:id",(req , res) => {
-    const id = Number(req.params.id);
-    const user = usersData.find((user)=> user.id === id);
-    if(user){
-        res.send(`<ul>
+// GEt user by id
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = usersData.find((user) => user.id === id);
+  if (user) {
+    res.send(`<ul>
                 <li>S.NO :${user.id} </li>
                 <li>First Name :${user.first_name} </li>
                 <li>Last Name :${user.last_name}</li>
                 <li>Email :${user.email}</li>
                 <li>Gender :${user.gender}</li>
                 <li>Job Type :${user.job_type}</li>
-            </ul>`)
-    }else{
-        res.send("No such user found ")
-    }
+            </ul>`);
+  } else {
+    res.send("No such user found ");
+  }
 });
 
 // GET all user in json fromate
 app.get("/api/users", (req, res) => {
-  res
-    .status(200)
-    .json({
-      success: true,
-      message: "you have successfull get users data",
-      data: usersData,
-    });
+  res.status(200).json({
+    success: true,
+    message: "you have successfull get users data",
+    data: usersData,
+  });
   req.end();
 });
 
-app.post("/api/users/", (req , res) => {
-    const body = req.body
-    const id = usersData.length + 1 
-    usersData.push({id , ...body})
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(usersData) , (err, data) => {
-        if(!err){
-            return res.status(201).json({status:"success" , id:id})
-        }else{
-            return res.status(500).json({status:"error" , message: err})
+app.post("/api/users/", (req, res) => {
+  User.create({
+    firstName
+  })
 
-        }
-    }
-    )
+ 
+});
 
-}
-)
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+app.listen(port || 5000, () => {
+  console.log(`server is running on port ${port}`);
 });
